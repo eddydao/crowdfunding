f2388c30a2e00062b4c8c1cc189cadcb5a55051d
dkthanh <daokythanh.343@gmail.com>
2020-10-15 21:14:47 +0700

    Add code for login form

---
+++ src/main/java/com/dkthanh/demo/util/EncryptedPasswordUtil.java
@@ -0,0 +1,11 @@
+package com.dkthanh.demo.util;
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+public class EncryptedPasswordUtil {
+
+    public static String encrytePassword(String password) {
+        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+        return encoder.encode(password);
+    }
+}
