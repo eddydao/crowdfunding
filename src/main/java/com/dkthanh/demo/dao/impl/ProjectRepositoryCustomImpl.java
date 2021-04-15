09907b78b0bb097ea98e8551748de8a96b325e51
Thanh Dao <daokythanh.343@gmail.com>
2021-04-16 00:03:31 +0700

    Fix sql of gettting full infomation of project

--- src/main/java/com/dkthanh/demo/dao/impl/ProjectRepositoryCustomImpl.java
+++ src/main/java/com/dkthanh/demo/dao/impl/ProjectRepositoryCustomImpl.java
@@ -39,21 +39,23 @@ public class ProjectRepositoryCustomImpl implements ProjectRepositoryCustom {
                         "  A.goal,\n" +
                         "  A.pledged,\n" +
                         "  A.investor_count,\n" +
-                        "  A.project_status_id,\n" +
                         "  A.recommended,\n" +
-                        "  0 as day_left,\n"+
+                        "  A.country_id,\n" +
                         "  B.material_id,\n" +
                         "  B.description,\n" +
                         "  B.path,\n" +
+                        "  A.project_status_id as status_id,\n"+
+                        "  D.name as status_name,\n"+
                         "  C.id as category_id,\n" +
                         "  C.name as category_name,\n" +
                         "  0.0 as percent_pledged,\n"+
-                        "  D.id as status_id,\n"+
-                        "  D.name as status_name\n"+
+                        "  0 as day_left,\n"+
+                        "  story_id as story_id\n"+
                         "FROM\n" +
                         "  project A left join material B on A.project_id = B.project_id\n" +
                         "  JOIN category C ON A.category_id = C.id\n" +
                         "  JOIN status D ON A.project_status_id = D.id\n" +
+                        "  JOIN story G ON A.project_id = G.project_id\n" +
                         "  JOIN user E ON A.user_id = E.id\n" +
                         "  JOIN user_detail F ON E.id = F.user_id\n"+
                         "WHERE 1 = 1\n" );
