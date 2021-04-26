d55e94159caadbf68000dd587a8e51093e834b9c
Thanh Dao <daokythanh.343@gmail.com>
2021-04-27 00:04:10 +0700

    change date picker for basic form
    change date picker
    add custom display function for duration field
    not map field

--- src/main/resources/static/creator/js/custom-proj.js
+++ src/main/resources/static/creator/js/custom-proj.js
@@ -1,4 +1,24 @@
 // custom js of dev
+
+$( document ).ready(function() {
+    $('#preview-img').attr('src', '../../images/bg-title-01.jpg');
+
+    var enddate_input=$('input[name="end-date"]');
+    var startdate_input=$('input[name="start-date"]');
+    var container=$('.bootstrap-iso form').length>0 ? $('.bootstrap-iso form').parent() : "body";
+    var options={
+        format: 'dd/mm/yyyy',
+        container: container,
+        todayHighlight: true,
+        autoclose: true,
+    };
+    enddate_input.datepicker(options);
+    startdate_input.datepicker(options);
+
+});
+
+
+
 var reader = new FileReader();
 reader.onload = function (e) {
     var src = e.target.result;
@@ -17,6 +37,16 @@ $("#file-input").change(function(){
     readURL(this);
 });
 
-$( document ).ready(function() {
-    $('#preview-img').attr('src', '../../images/bg-title-01.jpg');
-});
+function showSpecEndDateInput() {
+    if (document.getElementById('fixed-day').checked) {
+        document.getElementById('inp-fixed-day').style.visibility = 'visible';
+    }
+    else { document.getElementById('inp-fixed-day').style.visibility = 'hidden'; }
+
+    if (document.getElementById('spec-date').checked) {
+        document.getElementById('inp-spec-date').style.visibility = 'visible';
+    }
+    else { document.getElementById('inp-spec-date').style.visibility = 'hidden';}
+
+}
+
