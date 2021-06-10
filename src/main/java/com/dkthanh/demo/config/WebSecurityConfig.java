4209b596e4cc29c41e4deca63dc02487da5fb0a6
Thanh Dao <daokythanh.343@gmail.com>
2021-06-10 22:49:54 +0700

    Change login UI

--- src/main/java/com/dkthanh/demo/config/WebSecurityConfig.java
+++ src/main/java/com/dkthanh/demo/config/WebSecurityConfig.java
@@ -45,20 +45,20 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable();
 
-        http.cors().and().authorizeRequests().antMatchers("/**").permitAll();
+//        http.cors().and().authorizeRequests().antMatchers("/**").permitAll();
 
-//        http.authorizeRequests().antMatchers("/","/index", "/logout", "/css/**", "/js/**"
-//                , "/img/**", "/project/**", "/images/**", "cdn.datatables.net/**", "/search", "/temp-result", "/signin", "/register").permitAll()
-//                .antMatchers("/creator/**").hasAnyRole("CREATOR", "ADMIN")
-//                .antMatchers("/admin/**").hasRole("ADMIN").anyRequest().authenticated()
-//        .and().formLogin()
-//                .loginProcessingUrl("/j_spring_security_check") // Submit URL
-//                .loginPage("/signin")
-//                .defaultSuccessUrl("/")
-//                .failureUrl("/register")
-//                .usernameParameter("username")
-//                .passwordParameter("password")
-//                .and().logout().logoutUrl("/logout").logoutSuccessUrl("/logoutSuccessful");
-//        http.authorizeRequests().and().exceptionHandling().accessDeniedPage("/403");
+        http.authorizeRequests().antMatchers("/","/index", "/logout", "/css/**", "/js/**"
+                , "/img/**", "/project/**", "/images/**", "cdn.datatables.net/**", "/search", "/temp-result", "/signin", "/register", "/login-assets/**").permitAll()
+                .antMatchers("/creator/**").hasAnyRole("CREATOR", "ADMIN")
+                .antMatchers("/admin/**").hasRole("ADMIN").anyRequest().authenticated()
+        .and().formLogin()
+                .loginProcessingUrl("/j_spring_security_check") // Submit URL
+                .loginPage("/signin")
+                .defaultSuccessUrl("/")
+                .failureUrl("/register")
+                .usernameParameter("username")
+                .passwordParameter("password")
+                .and().logout().logoutUrl("/logout").logoutSuccessUrl("/logoutSuccessful");
+        http.authorizeRequests().and().exceptionHandling().accessDeniedPage("/403");
     }
 }
