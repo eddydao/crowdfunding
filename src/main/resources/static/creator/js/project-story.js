1c229f907a7495e8b3ce5402bb20f4c166241b39
Thanh Dao <daokythanh.343@gmail.com>
2021-08-09 01:27:08 +0700

    move project story js function to project-story.js file

--- src/main/resources/static/creator/js/project-story.js
+++ src/main/resources/static/creator/js/project-story.js
@@ -2,3 +2,25 @@ function returnToOverviewPage(){
     var project_id = $("#inp_project_id").val();
     window.location.href = "/creator/project/" + project_id;
 }
+
+$( document ).ready(function() {
+
+    $('#editor').summernote({
+        height: 550,
+        maxHeight: 550,
+        toolbar: [
+            ['style', ['style']],
+            ['font', ['bold', 'underline', 'italic','clear']],
+            ['fontname', ['fontname']],
+            ['color', ['color']],
+            ['para', ['ul', 'ol', 'paragraph']],
+            ['table', ['table']],
+            ['insert', ['link', 'picture', 'video']]
+        ],
+        callbacks:{
+            onImageUpload: function(image){
+                uploadImage(image[0], projectId);
+            }
+        }
+    });
+});
